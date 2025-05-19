935f0aa635b1332af2cebf69a13919a95fd58421
Avanish-Gupta <avanish.gupta.official1@gmail.com>
2025-05-19 08:37:30 +0530

    LL practice mac

---
+++ LinkedList/Implementation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int value;
+        Node *next;
+
+        Node(int value)
+        {
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+int main(){
+
+
+    return 0;
+}
