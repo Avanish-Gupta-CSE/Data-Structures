9fb2747cf157527ff84a75721686f8ab1f06e569
Avanish Kumar Gupta <91612116+Avanish-Gupta-CSE@users.noreply.github.com>
2025-06-13 09:29:10 +0530

    STL: Pair

---
+++ STL/basics.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainPair()
+{
+  pair<int, string> student = {101, "Avanish"};
+
+  cout << "Pair->first: " << student.first << "\nPair->second: " << student.second << endl;
+}
+
+void explain int main()
+{
+  explainPair();
+  return 0;
+}
